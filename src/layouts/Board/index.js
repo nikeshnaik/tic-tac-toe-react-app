9d80a090ac7c93c2df48fc952c10f6aced27466d
nikeshnaik <nikeshnaik.dev@gmail.com>
2022-04-08 13:32:42 +0530

    feat(lock mouseclick): diables mouseclick and hover after player input, svg color update

--- src/layouts/Board/index.js
+++ src/layouts/Board/index.js
@@ -6,6 +6,7 @@ import { checkWinner, getNextPlayer } from '../../Game/TicTacToe_helpers'
 
 function Board(props) {
 
+
     let dimensions = {
         "width": props.width,
         "height": props.height
@@ -28,6 +29,7 @@ function Board(props) {
         props.setGameState({ ...updatedState })
 
 
+
     }
 
 
@@ -35,15 +37,15 @@ function Board(props) {
     return (
 
         <div className={styles.gridParent} style={dimensions}>
-            <div className={styles.gridChild1} data-gridindex="0" data-loc="[0,0]" onClick={handleMouseClick} > <Square icon={props.gameState.grid[0][0]} player={props.gameState.currentPlayer} /> </div>
-            <div className={styles.gridChild2} data-gridindex="1" data-loc="[0,1]" onClick={handleMouseClick}  > <Square icon={props.gameState.grid[0][1]} player={props.gameState.currentPlayer} /> </div>
-            <div className={styles.gridChild3} data-gridindex="2" data-loc="[0,2]" onClick={handleMouseClick} > <Square icon={props.gameState.grid[0][2]} player={props.gameState.currentPlayer} /> </div>
-            <div className={styles.gridChild4} data-gridindex="3" data-loc="[1,0]" onClick={handleMouseClick} ><Square icon={props.gameState.grid[1][0]} player={props.gameState.currentPlayer} />  </div>
-            <div className={styles.gridChild5} data-gridindex="4" data-loc="[1,1]" onClick={handleMouseClick} > <Square icon={props.gameState.grid[1][1]} player={props.gameState.currentPlayer} /> </div>
-            <div className={styles.gridChild6} data-gridindex="5" data-loc="[1,2]" onClick={handleMouseClick} ><Square icon={props.gameState.grid[1][2]} player={props.gameState.currentPlayer} />  </div>
-            <div className={styles.gridChild7} data-gridindex="6" data-loc="[2,0]" onClick={handleMouseClick} > <Square icon={props.gameState.grid[2][0]} player={props.gameState.currentPlayer} /> </div>
-            <div className={styles.gridChild8} data-gridindex="7" data-loc="[2,1]" onClick={handleMouseClick} ><Square icon={props.gameState.grid[2][1]} player={props.gameState.currentPlayer} />  </div>
-            <div className={styles.gridChild9} data-gridindex="8" data-loc="[2,2]" onClick={handleMouseClick} > <Square icon={props.gameState.grid[2][2]} player={props.gameState.currentPlayer} /> </div>
+            <div className={styles.gridChild1} data-gridindex="0" data-loc="[0,0]" onClick={props.gameState.grid[0][0] === -1 ? handleMouseClick : null} > <Square icon={props.gameState.grid[0][0]} player={props.gameState.currentPlayer} /> </div>
+            <div className={styles.gridChild2} data-gridindex="1" data-loc="[0,1]" onClick={props.gameState.grid[0][1] === -1 ? handleMouseClick : null}  > <Square icon={props.gameState.grid[0][1]} player={props.gameState.currentPlayer} /> </div>
+            <div className={styles.gridChild3} data-gridindex="2" data-loc="[0,2]" onClick={props.gameState.grid[0][2] === -1 ? handleMouseClick : null} > <Square icon={props.gameState.grid[0][2]} player={props.gameState.currentPlayer} /> </div>
+            <div className={styles.gridChild4} data-gridindex="3" data-loc="[1,0]" onClick={props.gameState.grid[1][0] === -1 ? handleMouseClick : null} ><Square icon={props.gameState.grid[1][0]} player={props.gameState.currentPlayer} />  </div>
+            <div className={styles.gridChild5} data-gridindex="4" data-loc="[1,1]" onClick={props.gameState.grid[1][1] === -1 ? handleMouseClick : null} > <Square icon={props.gameState.grid[1][1]} player={props.gameState.currentPlayer} /> </div>
+            <div className={styles.gridChild6} data-gridindex="5" data-loc="[1,2]" onClick={props.gameState.grid[1][2] === -1 ? handleMouseClick : null} ><Square icon={props.gameState.grid[1][2]} player={props.gameState.currentPlayer} />  </div>
+            <div className={styles.gridChild7} data-gridindex="6" data-loc="[2,0]" onClick={props.gameState.grid[2][0] === -1 ? handleMouseClick : null} > <Square icon={props.gameState.grid[2][0]} player={props.gameState.currentPlayer} /> </div>
+            <div className={styles.gridChild8} data-gridindex="7" data-loc="[2,1]" onClick={props.gameState.grid[2][1] === -1 ? handleMouseClick : null} ><Square icon={props.gameState.grid[2][1]} player={props.gameState.currentPlayer} />  </div>
+            <div className={styles.gridChild9} data-gridindex="8" data-loc="[2,2]" onClick={props.gameState.grid[2][2] === -1 ? handleMouseClick : null} > <Square icon={props.gameState.grid[2][2]} player={props.gameState.currentPlayer} /> </div>
         </div >
 
     );
